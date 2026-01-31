c1335dca0728928572271b7859fe76711792f901
Adriano Avelino <adriano-st777@hotmail.com>
2026-01-31 16:34:22 -0300

    adiciona projeto incial do spring

---
+++ spring/src/test/java/br/com/estudo/spring_cucumber/SpringCucumberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.spring_cucumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCucumberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
